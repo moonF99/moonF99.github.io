dfd168db7ddc5d5b735ead8d68c924ba95f91d0e
moonF99 <lrf11344@gmailcom>
2026-09-25 21:33:46 +0800

    Add Utterances comments to blog posts

---
+++ scripts/comments.js
@@ -0,0 +1,20 @@
+/* Load Utterances on individual article pages. */
+(() => {
+  const container = document.querySelector(".post-body .post-comment");
+  if (!container || container.querySelector("script[data-utterances]")) return;
+
+  const heading = document.createElement("h2");
+  heading.className = "post-comment-title";
+  heading.textContent = "评论区";
+  container.prepend(heading);
+
+  const script = document.createElement("script");
+  script.src = "https://utteranc.es/client.js";
+  script.async = true;
+  script.crossOrigin = "anonymous";
+  script.dataset.utterances = "true";
+  script.setAttribute("repo", "moonf99/moonf99.github.io");
+  script.setAttribute("issue-term", "pathname");
+  script.setAttribute("theme", "preferred-color-scheme");
+  container.appendChild(script);
+})();
